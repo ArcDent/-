1b399b96f7db4154b7aa38522c1e25c085bdc66a
ArcDent <yanganyi823@gmail.com>
2025-03-06 10:47:00 +0800

    更新 绿色特供屏蔽词.user.js

--- JS-油猴脚本/绿色特供屏蔽词.user.js
+++ JS-油猴脚本/绿色特供屏蔽词.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         安逸￥屏蔽词￥取消记住密码￥绿色特供版
-// @version      1.0.3
+// @version      1.0.4
 // @downloadURL  https://raw.githubusercontent.com/ArcDent/-/refs/heads/main/JS-油猴脚本/绿色特供屏蔽词.user.js
 // @updateURL    https://raw.githubusercontent.com/ArcDent/-/refs/heads/main/JS-油猴脚本/绿色特供屏蔽词.user.js
 // @match        https://3dtank.com/play/
